e641899611dbfe2e35c99ae644d1e63ef0e60a4a
Evgeniya <jennie.96@mail.ru>
2021-11-02 09:25:41 +0600

    added meeting to urls

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, test1
+from main.views import homepage, test, test1, meeting
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', homepage, name='home'),
     path('test/', test, name='test'),
     path('test1/', test1, name='test1'),
+    path('meeting/', meeting, name='meeting'),
  ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
