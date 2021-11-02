20c8075c2fb49b998006c0a29092d7ddd0feb59e
Evgeniya <jennie.96@mail.ru>
2021-11-02 08:03:05 +0600

    (Added todo model and ToMeet model and in admin

--- todo/main/models.py
+++ todo/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    test = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favourite = models.BooleanField(default=False)
+
+class ToMeet(models.Model):
+    persone = models.TextField()
+    phone_number = models.CharField(max_length=50)
+    date_of_meeting = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
